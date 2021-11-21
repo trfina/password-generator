2e390641fa8cbc97b066c76323264af20c08936e
Tammy Fina <finatammy@gmail.com>
2021-11-21 10:29:33 -0500

    errant comma in code removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,7 +102,7 @@ function writePassword() {
       } 
       else {
         invalidPassword = false;
-        /console.log(invalidPassword);
+        //console.log(invalidPassword);
       }
     }
     else {
